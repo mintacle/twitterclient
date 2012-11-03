4cd0d3af5e13c8537b29a8154a94e72a8022d39a
Katrina Junkin <katrinajunkin@gmail.com>
2012-11-02 21:53:45 -0400

    part one of twitter client

--- twitterhack.py
+++ twitterhack.py
@@ -1,2 +1,18 @@
-#twitterhack.py
-#kfdjdjk
+import urllib
+import json
+import sys
+
+def search_twitter(query='ermergerd'):
+  url = 'http://search.twitter.com/search.json?q=' + query
+  response = urllib.urlopen(url).read()
+  data = json.loads(response)
+  return data['results']
+
+def print_tweets(tweets):
+  for tweet in tweets:
+    print tweet['from_user'] + ': ' + tweet['text'] + '\n'
+
+if __name__ == "__main__":
+  query = sys.argv[1]
+  results = search_twitter(query)
+  print_tweets(results)
