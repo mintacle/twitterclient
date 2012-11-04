b687101e16c6d6fe81bd5a843b7c093cae817c4f
Katrina Junkin <katrinajunkin@gmail.com>
2012-11-03 21:30:14 -0400

    testing thing

--- twitterhack.py
+++ twitterhack.py
@@ -2,11 +2,17 @@ import urllib
 import json
 import sys
 
-def search_twitter(query='ermergerd'):
+def search_twitter(query='mailchimp'):
   url = 'http://search.twitter.com/search.json?show_user-true&q=' + query
   response = urllib.urlopen(url).read()
   data = json.loads(response)
   return data['results']
+  
+def search_user(query):
+  url = 'http://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&screen_name=' + query
+  response = urllib.urlopen(url).read()
+  data = json.loads(response)
+  return data
 
 def print_tweets(tweets):
   for tweet in tweets:
